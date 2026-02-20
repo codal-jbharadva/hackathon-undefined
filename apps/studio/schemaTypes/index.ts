323cda8a289824554207d9feff3b0f9b79c33564
codal-jbharadva <jbharadva@codal.com>
2026-02-21 00:49:12 +0530

    Initial test document type create

--- apps/studio/schemaTypes/index.ts
+++ apps/studio/schemaTypes/index.ts
@@ -1,3 +1,4 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import { postType } from './postType'
 
-export const schemaTypes: SchemaTypeDefinition[] = []
+export const schemaTypes: SchemaTypeDefinition[] = [postType]
